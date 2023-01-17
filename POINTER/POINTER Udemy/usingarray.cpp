cee969c42a6cff3225e249342d7fbf170d93d477
Aditya <adityavishal903@gmail.com>
2023-01-17 21:53:22 +0530

    2

--- POINTER/POINTER Udemy/usingarray.cpp
+++ POINTER/POINTER Udemy/usingarray.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 #include<stdio.h>
+using namespace std;
 
 int main(){
-    int a[5]={1,5,4,5,8};
+    int A[5]={1,5,4,5,8};
     int*p;
-    p=a;
+    p=A;
+    for(int i=0;i<=5;i++)
+    //cout<<A[i]<<endl;
+    cout<<p[i]<<endl;
 
     return 0;
 }
