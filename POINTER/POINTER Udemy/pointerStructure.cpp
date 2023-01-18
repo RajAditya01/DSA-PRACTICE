635e73b76d516a5dc279dc85d1ccdcc5af12b5bd
Aditya <adityavishal903@gmail.com>
2023-01-18 16:00:59 +0530

    practice Pointer

---
+++ POINTER/POINTER Udemy/pointerStructure.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+struct rectangle
+{
+    int length;
+    int breadth;
+};
+
+int main(){
+    rectangle r={10,5};
+    cout<<r.length<<endl;
+    cout<<r.breadth<<endl;
+
+
+    rectangle *p=&r;
+    cout<<p->length<<endl;
+    cout<<p->breadth<<endl;
+
+    return 0;
+}
