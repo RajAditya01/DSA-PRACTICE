c6317895fb4e1ba606ced51c97636882d86582e3
Aditya <adityavishal903@gmail.com>
2023-02-02 22:10:31 +0530

    Insertion Sort

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,28 +1,27 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
-void insertionSort(int n, vector<int> &arr){
-    
-    for(int i = 1; i<n; i++) {
-        int temp = arr[i];
-        int j = i-1;
-        for(; j>=0; j--) {
-            
-            if(arr[j] > temp) {
-                //shift
-                arr[j+1] = arr[j];
-            }
-            else { // ruk jao
-                break;
-            }
-            
-        }
-        //copy temp value
-        arr[j+1] = temp;  
-    } 
-}
 
 int main(){
-    nsertionSort()
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for( int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=1; i<n; i++){
+        int current=arr[i];
+        int j=i-1;
+
+        while(arr[j]>current && j>=0){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=current;
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
 }
+
