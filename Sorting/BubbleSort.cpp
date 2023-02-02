dc5c9d58340827be131156f659990f0bf164a0e3
Aditya <adityavishal903@gmail.com>
2023-02-02 22:25:14 +0530

    Bubble sort

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int arr[] = {6, 4, 9, 1, 4, 2};
+    int arr[6] = {6, 4, 9, 1, 4, 2};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     for(int i = 0; i < size - 1; i++){
