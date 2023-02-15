f529ac9a0f4523d28c18f6840dc1b8cc1965eca6
Aditya <adityavishal903@gmail.com>
2023-02-15 23:37:49 +0530

    4

--- Coding-Ninja/FindAddress.cpp
+++ Coding-Ninja/FindAddress.cpp
@@ -6,11 +6,18 @@ int main(){
     // int *a = &b;
     // cout<<a;
 
-    int a = 100, b = 200;
-    int *p = &a, *q = &b;
-    p = q;
-    cout<<p<<endl;
-    cout<<q;
+    // int a = 100, b = 200;
+    // int *p = &a, *q = &b;
+    // p = q;
+    // cout<<p<<endl;
+    // cout<<q;
+
+    int a = 7;
+    int b = 17;
+    int *c = &b;
+    *c = 7;
+    cout  << a << " " << b << endl;
+
 
 }
 
